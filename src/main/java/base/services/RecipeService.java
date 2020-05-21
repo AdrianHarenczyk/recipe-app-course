5937a091d623e6665b7d45b4f14e7620351c0c67
Adrian Hareńczyk <adrian_harenczyk@epam.com>
2020-05-21 08:39:16 +0200

    Added recipe service and its implementation. Closes #14.

---
+++ src/main/java/base/services/RecipeService.java
@@ -0,0 +1,11 @@
+package base.services;
+
+import base.model.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+}
---
+++ src/main/java/base/services/impl/RecipeServiceImpl.java
@@ -0,0 +1,28 @@
+package base.services.impl;
+
+import base.model.Recipe;
+import base.repositories.RecipeRepository;
+import base.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getRecipes() {
+        Set<Recipe> recipes = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
+        return recipes;
+    }
+}
