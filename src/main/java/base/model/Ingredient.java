32c7c3dad7e7f3e8091da99faba16c87f03cefd8
Adrian Hareńczyk <adrian_harenczyk@epam.com>
2020-05-21 07:53:52 +0200

    Added Unit of measure model and enhanced Ingredient with new field. Closes #6.

--- src/main/java/base/model/Ingredient.java
+++ src/main/java/base/model/Ingredient.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
 @Entity
@@ -15,6 +16,8 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
+    @OneToOne
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -36,6 +39,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {
