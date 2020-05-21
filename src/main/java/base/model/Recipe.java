41e6663a1f8ad5c7e88f507a834f2e749f641bcc
Adrian Hareńczyk <adrian_harenczyk@epam.com>
2020-05-21 07:36:06 +0200

    Add recipe model pojo. Closes #2.

---
+++ src/main/java/base/model/Recipe.java
@@ -0,0 +1,14 @@
+package base.model;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Byte[] image;
+
+}
