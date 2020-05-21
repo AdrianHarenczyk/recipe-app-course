72bdce658ec003b2d2e26ddae50a0430083047c5
Adrian Hareńczyk <adrian_harenczyk@epam.com>
2020-05-21 07:28:19 +0200

    Add index page. Closes #1.

---
+++ src/main/java/base/RecipeAppCourseApplication.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppCourseApplication.class, args);
+	}
+
+}
