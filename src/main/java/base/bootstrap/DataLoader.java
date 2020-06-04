32b14c48175b0c1bdc96381dc7d0f389ecb2a6b3
Adrian Hareńczyk <adrian_harenczyk@epam.com>
2020-06-04 07:07:16 +0200

    Added Transactional annotation. Closes #19.

--- src/main/java/base/bootstrap/DataLoader.java
+++ src/main/java/base/bootstrap/DataLoader.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -36,6 +37,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
     }
